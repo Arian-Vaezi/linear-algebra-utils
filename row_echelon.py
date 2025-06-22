2ddc32891eddd5ef79c78c58b6e306d6780eab26
arian <arianvzy@gmail.com>
2025-06-22 17:43:29 +0200

    Completed the non zero founding function

--- row_echelon.py
+++ row_echelon.py
@@ -7,3 +7,9 @@ import numpy as np
 from typing import List, Tuple
 def swap_rows(M: np.array, i: int, j: int)-> None:
     M[[i,j]]=M[[j,i]]
+def find_nonzero_row(M: np,array, col, start_row)->None:
+    numberOfRow=M.shape()
+    for i in range(start_row, numberOfRow[0]):
+        if M[i,col]!=0:
+         return i
+    return None
