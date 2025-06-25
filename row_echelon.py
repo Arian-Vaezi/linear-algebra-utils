1d1cecc7396054fbd9faab9116f25353f7fb36f5
arian <arianvzy@gmail.com>
2025-06-25 14:11:50 +0200

    adding reduced form

--- row_echelon.py
+++ row_echelon.py
@@ -23,10 +23,11 @@ def scale_row(M: np.array,i:int , factor: float):
       raise ValueError("scale factor must be non-zero")
    M[i]=factor*M[i]
 
-def row_echelon(M: np.array):
+def row_echelon(M: np.array)->Tuple[np.ndarray,list[int]]:
     A=M.copy()
     n_row, n_cols=M.shape
     pivot_row=0
+    pivot_cols=[]
 
     for col in range (n_cols):
        r=find_nonzero_row(A, col, pivot_row)
@@ -34,17 +35,35 @@ def row_echelon(M: np.array):
         continue
        if r!=pivot_row:
           swap_rows(A,r,pivot_row)
+       pivot_cols.append(col)
        factor=1.0/A[pivot_row,col]
        scale_row(A,pivot_row,factor)
        for r2 in range(pivot_row+1,n_row):
           scale=-A[r2,col]
           add_scaled_row(A,pivot_row,r2,scale)
        pivot_row+=1
-    return A
-M= np.array([[4,2,3]
-              ,[2,5,6]
-              ,[3,8,9]], float)
+    return A,pivot_cols
 
-M=row_echelon(M)
-print(M)
+def rref(M:np.array)->np.array:
+   E, pivot_cols= row_echelon(M)
+   r=len(pivot_cols)
+   for k in range(r-1,-1,-1):
+      col=pivot_cols[k]
+      for i in range(k):
+         if E[i,col]!=0:
+            scale=-E[i,col]
+            add_scaled_row(E,k,i,scale)
+      factor=1.0/E[k,col]  #renormalizing 
+      scale_row(E,k,factor)
+   return E
+            
+
+M= np.array([[4,1,1]
+              ,[1,1,2]
+              ,[1,1,3]], float)
+
+# M=row_echelon(M)
+RM= rref(M)
+print(RM)
+
 
